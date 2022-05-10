4033d6b5765e87157a6700c3a5e9b007f874155c
AbdlefattahOmar <abdlefattahomar66@gmail.com>
2022-05-11 00:38:16 +0200

    fixing urls issue

--- blog/urls.py
+++ blog/urls.py
@@ -17,8 +17,5 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-     path('', include('posts.urls')),
-    path('users/', include('users.urls')),
-    path('manager/', include('manager.urls')),
+    path('admin/', admin.site.urls),    
 ]
