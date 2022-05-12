12b1eae42ea256b78eb37391de3a3e14398350a7
Mahmoud Majid <76546155+Mahmoud-Majid@users.noreply.github.com>
2022-05-12 03:38:19 +0200

    add admin view

--- blog/settings.py
+++ blog/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djangoproject',
-        'USER': 'reem',
-        'PASSWORD': 'rimoura95',
+        'USER': 'root',
+        'PASSWORD': '0000',
         'HOST': 'localhost',
         'PORT': '3306',
     }
--- manager/urls.py
+++ manager/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path("admins/delete/<int:id>", views.delete_admin, name="delete_admin"),
+    path("admins/", views.admins, name="admins"),
     path("admins/show/<int:id>", views.show, name="show_admin"),
 ]
