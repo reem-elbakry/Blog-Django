2cab8abc68e103c6a4a9a87d8b9fc3b66f2a0011
MarwaEzzatMo <marwaezzat201@gmail.com>
2022-05-13 10:09:32 +0200

    ..

--- blog/urls.py
+++ blog/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('manager/', include('manager.urls')),
-    # path('users/', include('users.urls')),
+    # path('manager/', include('manager.urls')),
+    path('users/', include('users.urls')),
 ]
